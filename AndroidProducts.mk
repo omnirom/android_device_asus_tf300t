7a39dafb9339e52968649efd5ec10bd2bc207df3
Menno van Grinsven <scanno71@gmail.com>
2013-10-14 23:00:50 +0200

    TF300T: Change the device tree to be able to start the build for Omni ROM

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,3 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/full_tf300t.mk
+    $(LOCAL_DIR)/full_tf300t.mk \
+    $(LOCAL_DIR)/custom_tf300t.mk
--- aosp_tf300t.mk
+++ aosp_tf300t.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2011 The Android Open Source Project
+# Copyright 2013 The Android Open-Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,5 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+$(call inherit-product, device/asus/grouper/custom_tf300t.mk)
 
-add_lunch_combo custom_tf300t-userdebug
+PRODUCT_NAME := aosp_tf300t
