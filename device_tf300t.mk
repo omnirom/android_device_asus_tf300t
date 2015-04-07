54a9c58d21066ad5edb7fe06b575151e37bda4f3
Menno van Grinsven <scanno71@gmail.com>
2015-04-07 22:16:28 +0200

    tf300t: Transformer devices do not have a telephony subsystem
    Change-Id: I3a1a8f914ef0f606d4ee98e200be7f451077d8c5

--- device_tf300t.mk
+++ device_tf300t.mk
@@ -115,7 +115,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # media files
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
     device/asus/tf300t/media_codecs.xml:system/etc/media_codecs.xml \
     device/asus/tf300t/media_profiles.xml:system/etc/media_profiles.xml \
